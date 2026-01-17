80002d6b8bd5f28d47838b20a58cd19e33d26f52
sapeksh123 <sapekshvishwakarma@gmail.com>
2026-01-17 09:51:24 +0530

    cors set

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ connectDB();
 app.use(cors({
     origin: [
         "http://localhost:5173",
+        "https://agumentik-task-seven.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
